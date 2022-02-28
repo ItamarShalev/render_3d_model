feb1e0c09b641619c88b422abf9c830a6141c83f
Itamar Shalev <itamar1111111@gmail.com>
2022-02-28 23:19:03 +0200

    unittests: polygon: Create and implements the tests.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

---
+++ src/test/java/com/playking/unittests/PolygonTests.java
@@ -0,0 +1,86 @@
+package com.playking.unittests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import com.playking.geometries.Polygon;
+import com.playking.primitives.Point;
+import com.playking.primitives.Vector;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Testing Polygons.
+ * @author Dan
+ */
+public class PolygonTests {
+
+    /**
+     * Test method for {@link Polygon#Polygon(Point...)}.
+     */
+    @Test
+    public void testConstructor() {
+        /* ============ Equivalence Partitions Tests ============== */
+
+        /* TC01: Correct concave quadrangular with vertices in correct order */
+        try {
+            new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0),
+                        new Point(-1, 1, 1));
+        } catch (IllegalArgumentException e) {
+            fail("Failed constructing a correct polygon");
+        }
+
+        /* TC02: Wrong vertices order */
+        assertThrows(IllegalArgumentException.class,
+                     () -> new Polygon(new Point(0, 0, 1), new Point(0, 1, 0), new Point(1, 0, 0),
+                                       new Point(-1, 1, 1)), //
+                     "Constructed a polygon with wrong order of vertices");
+
+        /* TC03: Not in the same plane */
+        assertThrows(IllegalArgumentException.class,
+                     () -> new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0),
+                                       new Point(0, 2, 2)), //
+                     "Constructed a polygon with vertices that are not in the same plane");
+
+        /* TC04: Concave quadrangular */
+        assertThrows(IllegalArgumentException.class,
+                     () -> new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0),
+                                       new Point(0.5, 0.25, 0.5)), //
+                     "Constructed a concave polygon");
+
+        /* =============== Boundary Values Tests ================== */
+
+        /* TC10: Vertex on a side of a quadrangular */
+        assertThrows(IllegalArgumentException.class,
+                     () -> new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0),
+                                       new Point(0, 0.5, 0.5)),
+                     "Constructed a polygon with vertix on a side");
+
+        /* TC11: Last point = first point */
+        assertThrows(IllegalArgumentException.class,
+                     () -> new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0),
+                                       new Point(0, 0, 1)),
+                     "Constructed a polygon with vertice on a side");
+
+        /* TC12: Co-located points */
+        assertThrows(IllegalArgumentException.class,
+                     () -> new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0),
+                                       new Point(0, 1, 0)),
+                     "Constructed a polygon with vertice on a side");
+    }
+
+    /**
+     * Test method for {@link Polygon#getNormal(Point)}.
+     */
+    @Test
+    public void testGetNormal() {
+        /* ============ Equivalence Partitions Tests ============== */
+        /* TC01: There is a simple single test here */
+        Polygon pl = new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0),
+                                 new Point(-1, 1, 1));
+        double sqrt3 = Math.sqrt(1d / 3);
+        assertEquals(new Vector(sqrt3, sqrt3, sqrt3), pl.getNormal(new Point(0, 0, 1)),
+                     "Bad normal to trinagle");
+    }
+}
