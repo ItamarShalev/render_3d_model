cd4cd63c6dea600164afb57444944da33df6ff4e
Itamar Shalev <itamar1111111@gmail.com>
2022-03-04 03:29:39 +0200

    geometries: tube: Implement getNormal function and cleanup.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- src/main/java/com/playking/geometries/Tube.java
+++ src/main/java/com/playking/geometries/Tube.java
@@ -2,6 +2,7 @@ package com.playking.geometries;
 
 import com.playking.primitives.Point;
 import com.playking.primitives.Ray;
+import com.playking.primitives.Util;
 import com.playking.primitives.Vector;
 
 /**
@@ -12,7 +13,16 @@ public class Tube implements Geometry {
     protected final Ray axisRay;
     protected final double radius;
 
+    /**
+     * Create tube.
+     * @param axisRay the ray contains the direction and the center point of the center
+     * @param radius the radius of the tube
+     * @throws IllegalArgumentException if the radius is negative or zero
+     */
     public Tube(Ray axisRay, double radius) {
+        if (radius < 0 || Util.isZero(radius)) {
+            throw new IllegalArgumentException("Radius can't be zero or negative.");
+        }
         this.axisRay = axisRay;
         this.radius = radius;
     }
@@ -27,6 +37,14 @@ public class Tube implements Geometry {
 
     @Override
     public Vector getNormal(Point point) {
-        return null;
+        Vector pMinusHead = point.subtract(axisRay.getP0());
+        double t = axisRay.getDir().dotProduct(pMinusHead);
+        /* Check if the point is "front" to the p0 the point in the base */
+        if (Util.isZero(t)) {
+            return pMinusHead.normalize();
+        }
+        /* The point on the side calculate the normal */
+        Point center = axisRay.getP0().add(axisRay.getDir().scale(t));
+        return point.subtract(center).normalize();
     }
 }
