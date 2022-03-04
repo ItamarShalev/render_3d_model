6608e653c02887543e09a6f702fcde8ddf34cf64
Itamar Shalev <itamar1111111@gmail.com>
2022-03-04 03:32:01 +0200

    geometries: triangle: Implement getNormal function and cleanup.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- src/main/java/com/playking/geometries/Triangle.java
+++ src/main/java/com/playking/geometries/Triangle.java
@@ -1,6 +1,7 @@
 package com.playking.geometries;
 
 import com.playking.primitives.Point;
+import com.playking.primitives.Vector;
 
 /**
  * Class describe triangle, polygon with 3 points.
@@ -16,5 +17,10 @@ public class Triangle extends Polygon {
      */
     public Triangle(Point p1, Point p2, Point p3) {
         super(p1, p2, p3);
+    }
+
+    @Override
+    public Vector getNormal(Point point) {
+        return super.getNormal(point);
     }
 }
