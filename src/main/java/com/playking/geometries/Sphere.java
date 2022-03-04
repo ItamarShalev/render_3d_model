2157d5bf500d61d42ab08d923095be478009c175
Itamar Shalev <itamar1111111@gmail.com>
2022-03-04 03:33:06 +0200

    geometries: sphere: Implement getNormal function and cleanup.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- src/main/java/com/playking/geometries/Sphere.java
+++ src/main/java/com/playking/geometries/Sphere.java
@@ -26,6 +26,6 @@ public class Sphere implements Geometry {
 
     @Override
     public Vector getNormal(Point point) {
-        return null;
+        return point.subtract(center).normalize();
     }
 }
