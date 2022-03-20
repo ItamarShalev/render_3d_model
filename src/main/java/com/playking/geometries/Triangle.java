30dbe1164480f4a5e0c011736a0e4fe26b888634
Itamar Shalev <itamar1111111@gmail.com>
2022-03-21 01:53:56 +0200

    geometries: triangle: Implement findIntersections function.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- src/main/java/com/playking/geometries/Triangle.java
+++ src/main/java/com/playking/geometries/Triangle.java
@@ -1,7 +1,10 @@
 package com.playking.geometries;
 
 import com.playking.primitives.Point;
+import com.playking.primitives.Ray;
 import com.playking.primitives.Vector;
+import java.util.List;
+
 
 /**
  * Class describe triangle, polygon with 3 points.
@@ -22,5 +25,45 @@ public class Triangle extends Polygon {
     @Override
     public Vector getNormal(Point point) {
         return super.getNormal(point);
+    }
+
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+        /* If ray doesn't intersect the plan consist in triangle return null */
+        if (plane.findIntersections(ray) == null) {
+            return null;
+        }
+        List<Point> result = null;
+        Vector vector, vector1, vector2, vector3;
+        Vector normal1, normal2, normal3;
+        Point point1, point2, point3;
+        boolean isThereIntersections;
+
+        vector = ray.getDir();
+
+        point1 = vertices.get(0);
+        point2 = vertices.get(1);
+        point3 = vertices.get(2);
+
+        /* Otherwise, check if ray intersect the triangle */
+        vector1 = point1.subtract(ray.getP0());
+        vector2 = point2.subtract(ray.getP0());
+        vector3 = point3.subtract(ray.getP0());
+
+        normal1 = vector1.crossProduct(vector2);
+        normal2 = vector2.crossProduct(vector3);
+        normal3 = vector3.crossProduct(vector1);
+
+        isThereIntersections = vector.dotProduct(normal1) > 0 && vector.dotProduct(normal2) > 0 &&
+                               vector.dotProduct(normal3) > 0;
+
+        isThereIntersections = isThereIntersections ||
+                               vector.dotProduct(normal1) < 0 && vector.dotProduct(normal2) < 0 &&
+                               vector.dotProduct(normal3) < 0;
+
+        if (isThereIntersections) {
+            result = plane.findIntersections(ray);
+        }
+        return result;
     }
 }
