b4fd7679c00da576c076aa270971f51201e9c227
Itamar Shalev <itamar1111111@gmail.com>
2022-03-21 01:51:27 +0200

    geometries: polygon: Cleanup - Change the comments style and etc.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- src/main/java/com/playking/geometries/Polygon.java
+++ src/main/java/com/playking/geometries/Polygon.java
@@ -45,46 +45,52 @@ public class Polygon implements Geometry {
      */
     public Polygon(Point... vertices) {
         if (vertices.length < 3) {
-            throw new IllegalArgumentException("A polygon can't have less than 3 vertices");
+            throw new IllegalArgumentException("ERROR: A polygon can't have less than 3 vertices");
         }
         this.vertices = List.of(vertices);
-        // Generate the plane according to the first three vertices and associate the
-        // polygon with this plane.
-        // The plane holds the invariant normal (orthogonal unit) vector to the polygon
+        /*
+         * Generate the plane according to the first three vertices and associate the
+         * polygon with this plane.
+         * The plane holds the invariant normal (orthogonal unit) vector to the polygon
+         */
         plane = new Plane(vertices[0], vertices[1], vertices[2]);
         if (vertices.length == 3) {
-            return; // no need for more tests for a Triangle
+            /*  no need for more tests for a Triangle */
+            return;
         }
 
         Vector n = plane.getNormal();
 
-        // Subtracting any subsequent points will throw an IllegalArgumentException
-        // because of Zero Vector if they are in the same point
+        /*
+         * Subtracting any subsequent points will throw an IllegalArgumentException
+         * because of Zero Vector if they are in the same point
+         */
         Vector edge1 = vertices[vertices.length - 1].subtract(vertices[vertices.length - 2]);
         Vector edge2 = vertices[0].subtract(vertices[vertices.length - 1]);
-
-        // Cross Product of any subsequent edges will throw an IllegalArgumentException
-        // because of Zero Vector if they connect three vertices that lay in the same
-        // line.
-        // Generate the direction of the polygon according to the angle between last and
-        // first edge being less than 180 deg. It is hold by the sign of its dot product
-        // with
-        // the normal. If all the rest consequent edges will generate the same sign -
-        // the
-        // polygon is convex ("kamur" in Hebrew).
+        /*
+         * Cross Product of any subsequent edges will throw an IllegalArgumentException
+         * because of Zero Vector if they connect three vertices that lay in the same
+         * line.
+         * Generate the direction of the polygon according to the angle between last and
+         * first edge being less than 180 deg. It is hold by the sign of its dot product
+         * with
+         * the normal. If all the rest consequent edges will generate the same sign -
+         * the
+         * polygon is convex ("kamur" in Hebrew).
+         */
         boolean positive = edge1.crossProduct(edge2).dotProduct(n) > 0;
         for (var i = 1; i < vertices.length; ++i) {
-            // Test that the point is in the same plane as calculated originally
+            /* Test that the point is in the same plane as calculated originally */
             if (!isZero(vertices[i].subtract(vertices[0]).dotProduct(n))) {
                 throw new IllegalArgumentException(
                     "All vertices of a polygon must lay in the same plane");
             }
-            // Test the consequent edges have
+            /* Test the consequent edges have */
             edge1 = edge2;
             edge2 = vertices[i].subtract(vertices[i - 1]);
             if (positive != (edge1.crossProduct(edge2).dotProduct(n) > 0)) {
                 throw new IllegalArgumentException(
-                    "All vertices must be ordered and the polygon must be convex");
+                    "ERROR: All vertices must be ordered and the polygon must be convex");
             }
         }
         size = vertices.length;
