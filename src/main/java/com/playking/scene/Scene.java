6356cf246aa6b1d11fe9ccdf53e1e639f4bd5520
Itamar Shalev <itamar1111111@gmail.com>
2022-03-23 23:25:20 +0200

    scene: scene: Change the builder style.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- src/main/java/com/playking/scene/Scene.java
+++ src/main/java/com/playking/scene/Scene.java
@@ -4,7 +4,6 @@ import com.playking.elements.AmbientLight;
 import com.playking.geometries.Geometries;
 import com.playking.geometries.Intersect;
 import com.playking.primitives.Color;
-import com.playking.primitives.Double3;
 
 /**
  * PDS class describe scene all the data about the scene.
@@ -15,42 +14,29 @@ public class Scene {
     public AmbientLight ambientLight;
     public Geometries geometries;
 
-    private Scene(String name) {
+    public Scene(String name) {
         this.name = name;
         background = Color.BLACK;
         ambientLight = new AmbientLight();
         geometries = new Geometries();
     }
 
-    public static class Builder {
-        private Scene scene = null;
 
-        public Builder(String name) {
-            scene = new Scene(name);
-        }
+    public Scene setBackground(Color color) {
+        background = color;
+        return this;
+    }
 
-        public Builder reset(String name) {
-            scene = new Scene(name);
-            return this;
-        }
 
-        public Builder setBackground(Color color) {
-            scene.background = color;
-            return this;
-        }
+    public Scene setAmbient(AmbientLight ambientLight) {
+        this.ambientLight = ambientLight;
+        return this;
+    }
 
-        public Builder setAmbient(Color color, Double3 kAmbient) {
-            scene.ambientLight = new AmbientLight(color, kAmbient);
-            return this;
+    public Scene addGeometry(Intersect geometries) {
+        if (geometries != null) {
+            this.geometries.add(geometries);
         }
-
-        public Builder addGeometry(Intersect geometries) {
-            scene.geometries.add(geometries);
-            return this;
-        }
-
-        public Scene build() {
-            return scene;
-        }
+        return this;
     }
 }
