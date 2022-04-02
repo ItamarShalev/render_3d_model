35ef186cfb241157bdedd08cb53684dd6d2d911c
Itamar Shalev <itamar1111111@gmail.com>
2022-04-02 21:47:23 +0300

    geometries: intersect: Add GeoPoint class.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- src/main/java/com/playking/geometries/Intersect.java
+++ src/main/java/com/playking/geometries/Intersect.java
@@ -15,4 +15,36 @@ public abstract class Intersect {
      * @return List of points that intersection with the object
      */
     public abstract List<Point> findIntersections(Ray ray);
+
+
+    /**
+     * GeoPoint contains the geometry and the point on the geometry.
+     */
+    public static class GeoPoint {
+        public final Geometry geometry;
+        public final Point point;
+
+        public GeoPoint(Geometry geometry, Point point) {
+            this.geometry = geometry;
+            this.point = point;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) {
+                return true;
+            }
+            if (!(obj instanceof GeoPoint)) {
+                return false;
+            }
+            GeoPoint other = (GeoPoint)obj;
+            boolean sameGeometryType = this.geometry.getClass().equals(other.geometry.getClass());
+            return sameGeometryType && this.point.equals(other.point);
+        }
+
+        @Override
+        public String toString() {
+            return "GeoPoint{" + "geometry=" + geometry + ", point=" + point + '}';
+        }
+    }
 }
