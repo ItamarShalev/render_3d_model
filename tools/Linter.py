c61b9386bef2ee981977f5cedfbc850110303bde
Itamar Shalev <itamar1111111@gmail.com>
2022-05-02 22:31:41 +0300

    tools: linter: Cleanup
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- tools/Linter.py
+++ tools/Linter.py
@@ -1,8 +1,8 @@
-from enum import Enum
 import os
-from lxml import etree
 import xml.etree.ElementTree as ET
+from enum import Enum
 from tabulate import tabulate
+
 
 class Color(Enum):
     NO_COLOR = "0m"
@@ -18,32 +18,25 @@ class Color(Enum):
 
     @staticmethod
     def color_text(text, color):
-        if text is None:
+        if not text:
             return None
         return "\033[" + str(color.value) + text + "\033[" + str(Color.NO_COLOR.value)
 
-
     @staticmethod
     def color_list(ls, color):
-        if ls is None:
+        if not ls:
             return None
-        for i in range(len(ls)):
-            ls[i] = Color.color_text(ls[i],color)
-        return ls
-
+        return [Color.color_text(x, color) for x in ls]
 
 
 class ErrorElement():
     def __init__(self, attrib):
         self.source = attrib["source"]
-        self.source =  self.source[self.source.rfind(".") + 1:]
+        self.source = self.source[self.source.rfind(".") + 1:]
         self.line = attrib["line"]
         self.severity = attrib["severity"].capitalize()
         self.message = attrib["message"]
-        if "column" in attrib.keys():
-            self.column = attrib["column"]
-        else:
-            self.column = None
+        self.column = attrib["column"] if "column" in attrib else None
 
     def is_error(self):
         return self.severity == "Error"
@@ -51,40 +44,33 @@ class ErrorElement():
     def get_string(self):
         data = ""
         data += f"Line: {self.line}\t"
-        if self.column is not None:
+        if self.column:
             data += f"Column: {self.column}\t"
         data += f"Severity: {self.severity}\t"
         data += f"Message: {self.message}\t"
         data += f"Source: {self.source}\t"
         return data
 
-    def to_list(self, color = None):
+    def to_list(self, color=None):
         ls = [self.line, self.column, self.severity, self.message, self.source]
-        if color is None:
-            return ls
-        else:
-            return Color.color_list(ls, color)
+        return Color.color_list(ls, color) if color else ls
 
     @staticmethod
-    def headers(color = None):
+    def headers(color=None):
         ls = ["Line", "Column", "Severity", "Message", "Source"]
-        if color is None:
-            return ls
-        else:
-            return Color.color_list(ls, color)
+        return Color.color_list(ls, color) if color else ls
 
 
 def parse_checkstyle_xml(file_name):
     try:
         tree = ET.parse(file_name)
     except Exception as e:
-        print(Color.color_text(e.__str__(), Color.LIGHT_RED))
+        print(Color.color_text(str(e), Color.LIGHT_RED))
         exit(1)
         return
     root = tree.getroot()
     files = {}
     there_was_an_error = False
-    name = ""
     for file in root:
         location = file.attrib["name"]
         files[location] = []
@@ -97,24 +83,27 @@ def parse_checkstyle_xml(file_name):
 
 def print_errors(files_arrays):
     print_something = False
-    if files_arrays is None or len(files_arrays) == 0:
+    if not files_arrays:
         return print_something
     for location_file, errors_array in files_arrays.items():
-        if len(errors_array) == 0:
+        if not errors_array:
             continue
         print(Color.color_text(f"File: {location_file}", Color.LIGHT_RED))
         ls = []
         for error_object in errors_array:
-            ls.append(error_object.to_list(Color.LIGHT_RED if error_object.is_error() else Color.YELLOW))
+            color = Color.LIGHT_RED if error_object.is_error() else Color.YELLOW
+            ls.append(error_object.to_list(color))
         print_something = True
-        table = tabulate(ls ,headers=ErrorElement.headers(Color.PURPLE), showindex=True, tablefmt="pretty",
-                         stralign="left")
+        table = tabulate(ls, headers=ErrorElement.headers(Color.PURPLE), showindex=True,
+                         tablefmt="pretty", stralign="left")
         print(table)
     return print_something
+
 
 def create_checkstyle_files():
     os.system("mvn clean -q")
     os.system("mvn checkstyle:checkstyle -q")
+
 
 def main():
     create_checkstyle_files()
@@ -127,6 +116,7 @@ def main():
         print(Color.color_text(f"Success CheckStyle Code, Check the warnings !", Color.YELLOW))
     else:
         print(Color.color_text(f"Success CheckStyle Code, Perfect !", Color.GREEN))
+
 
 if __name__ == '__main__':
     main()
