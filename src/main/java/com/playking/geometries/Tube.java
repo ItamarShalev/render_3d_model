7bcc505fe4f407b9ffdcb75ac0624acc8ad106ec
Binyamin Chouraqi <bchouraq@j.jct.ac.il>
2022-05-10 23:48:44 +0300

    geometries: Add intersections between cylinder and tube to ray.
    Bonus for exercise 3.
    
    Signed-off-by: Binyamin Chouraqi <bchouraq@j.jct.ac.il>

--- src/main/java/com/playking/geometries/Tube.java
+++ src/main/java/com/playking/geometries/Tube.java
@@ -1,8 +1,10 @@
 package com.playking.geometries;
 
+import static com.playking.primitives.Util.alignZero;
+import static com.playking.primitives.Util.isZero;
+
 import com.playking.primitives.Point;
 import com.playking.primitives.Ray;
-import com.playking.primitives.Util;
 import com.playking.primitives.Vector;
 import java.util.List;
 
@@ -21,7 +23,7 @@ public class Tube extends Geometry {
      * @throws IllegalArgumentException if the radius is negative or zero
      */
     public Tube(Ray axisRay, double radius) {
-        if (radius < 0 || Util.isZero(radius)) {
+        if (radius < 0 || isZero(radius)) {
             throw new IllegalArgumentException("Radius can't be zero or negative.");
         }
         this.axisRay = axisRay;
@@ -41,7 +43,7 @@ public class Tube extends Geometry {
         Vector pMinusHead = point.subtract(axisRay.getP0());
         double t = axisRay.getDir().dotProduct(pMinusHead);
         /* Check if the point is "front" to the p0 the point in the base */
-        if (Util.isZero(t)) {
+        if (isZero(t)) {
             return pMinusHead.normalize();
         }
         /* The point on the side calculate the normal */
@@ -51,6 +53,64 @@ public class Tube extends Geometry {
 
     @Override
     public List<GeoPoint> findGeoIntersectionsHelper(Ray ray) {
+        Vector v = ray.getDir();
+        Vector v0 = axisRay.getDir();
+
+        /* Calculating temp1 = v - v0 * (v,v0) */
+        Vector temp1 = v;
+        double vv0 = v.dotProduct(v0);
+        if (!isZero(vv0)) {
+            Vector v0vv0 = v0.scale(vv0);
+            if (v.equals(v0vv0)) {
+                return null;
+            }
+            temp1 = v.subtract(v0vv0);
+        }
+
+        /* Calculating temp2 = dp - v0 * (dp,v0) where dp = p0 - p */
+        double temp1DotTemp2 = 0;
+        double squaredTemp2 = 0;
+        if (!ray.getP0().equals(axisRay.getP0())) {
+            Vector dp = ray.getP0().subtract(axisRay.getP0());
+            Vector temp2 = dp;
+            double dpv0 = dp.dotProduct(v0);
+            if (isZero(dpv0)) {
+                temp1DotTemp2 = temp1.dotProduct(temp2);
+                squaredTemp2 = temp2.lengthSquared();
+            } else {
+                Vector v0dpv0 = v0.scale(dpv0);
+                if (!dp.equals(v0dpv0)) {
+                    temp2 = dp.subtract(v0dpv0);
+                    temp1DotTemp2 = temp1.dotProduct(temp2);
+                    squaredTemp2 = temp2.lengthSquared();
+                }
+            }
+        }
+
+        /* Getting the quadratic equation: at^2 +bt + c = 0 */
+        double a = temp1.lengthSquared();
+        double b = 2 * temp1DotTemp2;
+        double c = alignZero(squaredTemp2 - radius * radius);
+
+        double squaredDelta = alignZero(b * b - 4 * a * c);
+        if (squaredDelta <= 0) {
+            return null;
+        }
+
+        double delta = Math.sqrt(squaredDelta);
+        double t1 = alignZero((-b + delta) / (2 * a));
+        double t2 = alignZero((-b - delta) / (2 * a));
+
+        if (t1 > 0 && t2 > 0) {
+            return List.of(new GeoPoint(this, ray.getP0(t1)), new GeoPoint(this, ray.getP0(t2)));
+        }
+        if (t1 > 0) {
+            return List.of(new GeoPoint(this, ray.getP0(t1)));
+        }
+        if (t2 > 0) {
+            return List.of(new GeoPoint(this, ray.getP0(t2)));
+        }
+
         return null;
     }
 }
--- src/test/java/com/playking/unittests/geometries/CylinderTests.java
+++ src/test/java/com/playking/unittests/geometries/CylinderTests.java
@@ -1,12 +1,13 @@
 package com.playking.unittests.geometries;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import com.playking.geometries.Cylinder;
 import com.playking.primitives.Point;
 import com.playking.primitives.Ray;
 import com.playking.primitives.Vector;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -65,5 +66,162 @@ public class CylinderTests {
         assertEquals(exceptVectorCenterBottomBase, cylinder.getNormal(bottomBaseCenterPoint),
                      "ERROR: getNormal() point in the center on the top base "
                      + "doesn't work correctly.");
+    }
+
+    /**
+     * Test method for {@link Cylinder#findIntersections(Ray)}.
+     */
+    @Test
+    void findIntersections() {
+        Ray ray = new Ray(new Point(2, 0, 0), new Vector(0, 0, 1));
+        Cylinder cylinder = new Cylinder(ray, 1d, 2d);
+
+        /* ============ Equivalence Partitions Tests ==============*/
+
+        /* TC01 ray is outside and parallel to the cylinder's ray */
+
+        ray = new Ray(new Point(5, 0, 0), new Vector(0, 0, 1));
+        List<Point> result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
+
+
+        /*TC02 ray starts inside and parallel to the cylinder's ray*/
+
+        ray = new Ray(new Point(2.5, 0, 1), new Vector(0, 0, 1));
+        result = cylinder.findIntersections(ray);
+        assertEquals(1, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(2.5, 0, 2)), result, "Bad intersection point");
+
+        /*TC03 ray starts outside and parallel to the cylinder's ray and crosses the cylinder*/
+
+        ray = new Ray(new Point(2.5, 0, -1), new Vector(0, 0, 1));
+        result = cylinder.findIntersections(ray);
+        assertEquals(2, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(2.5, 0, 0), new Point(2.5, 0, 2)), result,
+                     "Bad intersection point");
+
+        /*TC04 ray starts from outside and crosses the cylinder*/
+
+        ray = new Ray(new Point(-2, 0, 0.5), new Vector(1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertEquals(2, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(3, 0, 0.5), new Point(1, 0, 0.5)), result,
+                     "Bad intersection points");
+
+        /*TC05 ray starts from inside and crosses the cylinder*/
+        ray = new Ray(new Point(1.5, 0, 0.5), new Vector(1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertEquals(1, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(3, 0, 0.5)), result, "Bad intersection points");
+
+        /*TC06 ray starts from outside the cylinder and doesn't cross the cylinder*/
+        ray = new Ray(new Point(5, 0, 0), new Vector(1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
+
+        /*TC07 ray starts from outside and crosses base and surface*/
+        ray = new Ray(new Point(1, 0, -1), new Vector(1, 0, 1));
+        result = cylinder.findIntersections(ray);
+        assertEquals(2, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(3, 0, 1), new Point(2, 0, 0)), result,
+                     "Bad intersection points");
+
+        /*TC08 ray starts from outside and crosses surface and base*/
+        ray = new Ray(new Point(4, 0, 2), new Vector(-1, 0, -1));
+        result = cylinder.findIntersections(ray);
+        assertEquals(2, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(3, 0, 1), new Point(2, 0, 0)), result,
+                     "Bad intersection points");
+
+
+        /* =============== Boundary Values Tests ================== */
+
+        /*TC09 ray is on the surface of the cylinder (not bases)*/
+        ray = new Ray(new Point(3, 0, 0), new Vector(0, 0, 1));
+        result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
+
+        /*TC10 ray is on the base of the cylinder and crosses 2 times*/
+        ray = new Ray(new Point(-1, 0, 0), new Vector(1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
+
+        /*TC11 ray is in center of the cylinder*/
+        ray = new Ray(new Point(2, 0, 0), new Vector(0, 0, 1));
+        result = cylinder.findIntersections(ray);
+        assertEquals(1, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(2, 0, 2)), result, "Bad intersection points");
+
+        /*TC12 ray is perpendicular to cylinder's ray and starts from outside the tube*/
+        ray = new Ray(new Point(-2, 0, 0.5), new Vector(1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertEquals(2, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(3, 0, 0.5), new Point(1, 0, 0.5)), result,
+                     "Bad intersection points");
+
+        /*TC13 ray is perpendicular to cylinder's ray and starts from inside cylinder (not center)*/
+        ray = new Ray(new Point(1.5, 0, 0.5), new Vector(1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertEquals(1, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(3, 0, 0.5)), result, "Bad intersection points");
+
+        /*TC14 ray is perpendicular to cylinder's ray and starts from the center of cylinder*/
+        ray = new Ray(new Point(2, 0, 0.5), new Vector(1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertEquals(1, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(3, 0, 0.5)), result, "Bad intersection points");
+
+        /*TC15 ray is perpendicular to cylinder and starts from the surface of cylinder to inside*/
+        ray = new Ray(new Point(1, 0, 0.5), new Vector(1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertEquals(1, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(3, 0, 0.5)), result, "Bad intersection points");
+
+        /*TC16 ray is perpendicular to cylinder and starts from the surface of cylinder to outside*/
+        ray = new Ray(new Point(3, 0, 0), new Vector(1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
+
+        /*TC17 ray starts from the surface to outside*/
+        ray = new Ray(new Point(3, 0, 0), new Vector(1, 1, 1));
+        result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
+
+        /*TC18 ray starts from the surface to inside*/
+        ray = new Ray(new Point(3, 0, 0.5), new Vector(-1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertEquals(1, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(1, 0, 0.5)), result, "Bad intersection point");
+
+        /*TC19 ray starts from the center*/
+        ray = new Ray(new Point(2, 0, 0), new Vector(1, 0, 1));
+        result = cylinder.findIntersections(ray);
+        assertEquals(1, result.size(), "Wrong number of points");
+        assertEquals(List.of(new Point(3, 0, 1)), result, "Bad intersection point");
+
+        /*TC20 prolongation of ray crosses cylinder*/
+        ray = new Ray(new Point(3, 0, 0), new Vector(1, 0, 0));
+        result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
+
+        /*TC21 ray is on the surface starts before cylinder*/
+        ray = new Ray(new Point(3, 0, -1), new Vector(0, 0, 1));
+        result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
+
+        /*TC22 ray is on the surface starts at bottom's base*/
+        ray = new Ray(new Point(3, 0, 0), new Vector(0, 0, 1));
+        result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
+
+        /*TC23 ray is on the surface starts on the surface*/
+        ray = new Ray(new Point(3, 0, 1), new Vector(0, 0, 1));
+        result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
+
+        /*TC24 ray is on the surface starts at top's base*/
+        ray = new Ray(new Point(3, 0, 2), new Vector(0, 0, 1));
+        result = cylinder.findIntersections(ray);
+        assertNull(result, "Wrong number of points");
     }
 }
